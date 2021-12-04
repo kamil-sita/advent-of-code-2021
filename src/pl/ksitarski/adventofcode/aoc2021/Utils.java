46b538f786caa0fcea0e9dd34a69700f514126f0
Sita <kamilsitarski98@gmail.com>
2021-12-04 23:10:27 +0100

    day 4 part 1

--- src/pl/ksitarski/adventofcode/aoc2021/Utils.java
+++ src/pl/ksitarski/adventofcode/aoc2021/Utils.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Utils {
 
@@ -25,5 +26,36 @@ public class Utils {
         }
 
         return lines;
+    }
+
+    public static class Pair<T, U> {
+        private final T t;
+        private final U u;
+
+        public Pair(T t, U u) {
+            this.t = t;
+            this.u = u;
+        }
+
+        public T getT() {
+            return t;
+        }
+
+        public U getU() {
+            return u;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair<?, ?> pair = (Pair<?, ?>) o;
+            return Objects.equals(t, pair.t) && Objects.equals(u, pair.u);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(t, u);
+        }
     }
 }
