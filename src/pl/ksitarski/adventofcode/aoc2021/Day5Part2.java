8b70efb379b9db64f3a166ec4d931d651fe66ea5
Sita <kamilsitarski98@gmail.com>
2021-12-05 10:31:47 +0100

    day 5 part 2

---
+++ src/pl/ksitarski/adventofcode/aoc2021/Day5Part2.java
@@ -0,0 +1,83 @@
+package pl.ksitarski.adventofcode.aoc2021;
+
+import pl.ksitarski.adventofcode.aoc2021.Utils.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class Day5Part2 implements Solution {
+    public static void main(String[] args) {
+        System.out.println(new Day5Part2().solve(Utils.readFile("day5.txt")));
+    }
+
+    @Override
+    public int solve(List<String> lines) {
+        Board board = new Board();
+
+        for (String line : lines) {
+            String[] coords = line.split(" -> ");
+            String[] firstCoords = coords[0].split(",");
+            String[] secondCoords = coords[1].split(",");
+            board.addLine(
+                    new Pair<>(Integer.parseInt(firstCoords[0]), Integer.parseInt(firstCoords[1])),
+                    new Pair<>(Integer.parseInt(secondCoords[0]), Integer.parseInt(secondCoords[1]))
+            );
+        }
+        return board.overlapCount();
+    }
+
+    public static class Board {
+        private final Map<Pair<Integer, Integer>, Integer> map = new HashMap<>();
+
+        public void addLine(Pair<Integer, Integer> from, Pair<Integer, Integer> to) {
+            if (Objects.equals(from.getT(), to.getT())) { //vertical
+                int x = from.getT();
+                int smallerY = Math.min(from.getU(), to.getU());
+                int biggerY = Math.max(from.getU(), to.getU());
+
+                for (int y = smallerY; y <= biggerY; y++) {
+                    putAtPoint(x, y);
+                }
+            } else if (Objects.equals(from.getU(), to.getU())) { //horizontal
+                int y = from.getU();
+                int smallerX = Math.min(from.getT(), to.getT());
+                int biggerX = Math.max(from.getT(), to.getT());
+
+                for (int x = smallerX; x <= biggerX; x++) {
+                    putAtPoint(x, y);
+                }
+            } else {
+                if (from.getT() > to.getT()) {
+                    Pair<Integer, Integer> swap = from;
+                    from = to;
+                    to = swap;
+                }
+                int yAdj = 0;
+                int yGrowth = to.getU() > from.getU() ? 1 : -1;
+                for (int x = from.getT(); x <= to.getT(); x++) {
+                    putAtPoint(x, from.getU() + yAdj);
+                    yAdj += yGrowth;
+                }
+            }
+        }
+
+        private void putAtPoint(int x, int y) {
+            Pair<Integer, Integer> key = new Pair<>(x, y);
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+
+        public int overlapCount() {
+            return (int) map.values()
+                    .stream()
+                    .filter(integer -> integer > 1)
+                    .count();
+        }
+    }
+
+}
