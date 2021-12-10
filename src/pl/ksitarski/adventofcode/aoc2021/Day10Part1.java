bb89f221a0c89fc70aed43a28d91d542913046f6
Sita <kamilsitarski98@gmail.com>
2021-12-10 20:51:23 +0100

    day 10 part 1

---
+++ src/pl/ksitarski/adventofcode/aoc2021/Day10Part1.java
@@ -0,0 +1,75 @@
+package pl.ksitarski.adventofcode.aoc2021;
+
+import java.util.*;
+
+
+public class Day10Part1 implements Solution {
+
+    private static final Set<Character> BEGINNING_CHARS = Set.of('(', '[', '{', '<');
+    private static final Map<Character, Integer> CHAR_MISMATCH_SCORING = Map.of(
+            ')', 3,
+            ']', 57,
+            '}', 1197,
+            '>', 25137
+    );
+    private static final Map<Character, Character> CORRESPONDING_CHARACTERS = Map.of(
+            '(', ')',
+            '[', ']',
+            '{', '}',
+            '<', '>'
+    );
+
+    public static void main(String[] args) {
+        System.out.println(new Day10Part1().solve(Utils.readFile("day10.txt")));
+    }
+
+    @Override
+    public long solve(List<String> lines) {
+        long syntaxErrorScore = 0;
+
+        for (String line : lines) {
+            AnalysisLine analysisLine = new AnalysisLine(line);
+            analysisLine.parse();
+            if (analysisLine.hasFailed()) {
+                syntaxErrorScore += CHAR_MISMATCH_SCORING.get(analysisLine.failedOnChar());
+            }
+        }
+
+        return syntaxErrorScore;
+    }
+
+    public static class AnalysisLine {
+        private final Stack<Character> parsingStack = new Stack<>();
+        private final String line;
+        private Character failedOnCharacter = null;
+
+        public AnalysisLine(String line) {
+            this.line = line;
+        }
+
+        public void parse() {
+            for (char c : line.toCharArray()) {
+                if (BEGINNING_CHARS.contains(c)) {
+                    parsingStack.add(c);
+                } else {
+                    char topmostChar = parsingStack.peek();
+                    char correspondingChar = CORRESPONDING_CHARACTERS.get(topmostChar);
+                    if (correspondingChar != c) {
+                        failedOnCharacter = c;
+                        return;
+                    }
+                    parsingStack.pop();
+                }
+            }
+        }
+
+        public boolean hasFailed() {
+            return failedOnCharacter != null;
+        }
+
+        public char failedOnChar() {
+            return failedOnCharacter;
+        }
+
+    }
+}
