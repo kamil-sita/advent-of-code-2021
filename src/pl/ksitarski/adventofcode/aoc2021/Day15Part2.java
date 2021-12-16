6cb9e7d7b46037807556b4f8086f2df6d7ea0204
Sita <kamilsitarski98@gmail.com>
2021-12-16 20:37:00 +0100

    day 15 part 2

---
+++ src/pl/ksitarski/adventofcode/aoc2021/Day15Part2.java
@@ -0,0 +1,105 @@
+package pl.ksitarski.adventofcode.aoc2021;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+import static pl.ksitarski.adventofcode.aoc2021.Utils.readFile;
+
+
+public class Day15Part2 implements Solution {
+
+    private static final boolean DEBUG = false;
+
+    public static void main(String[] args) {
+        System.out.println(new Day15Part2().solve(readFile("day15.txt")));
+    }
+
+    @Override
+    public long solve(List<String> lines) {
+        int width = lines.get(0).length();
+        int height = lines.size();
+
+        Utils.Map2d<Integer> map2d = new Utils.Map2d<>();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                importMap(lines, map2d, i, j, width, height);
+            }
+        }
+
+        PriorityQueue<PathPoint> priorityQueue = new PriorityQueue<>();
+        Set<Utils.Coords> analyzed = new HashSet<>();
+        priorityQueue.add(new PathPoint(0, new Utils.Coords(0, 0)));
+
+        while (true) {
+            PathPoint pathPoint = priorityQueue.poll();
+
+            if (pathPoint.coords.equals(new Utils.Coords(map2d.getHeight() - 1, map2d.getWidth() - 1))) {
+                return pathPoint.cost;
+            }
+
+            int pathCost = pathPoint.cost;
+
+            Utils.Coords coordsDown = pathPoint.coords.withYDiff(1);
+            map2d.get(coordsDown).ifPresent(tileCost -> {
+                addIfNotYetAnalyzed(priorityQueue, analyzed, pathCost, coordsDown, tileCost);
+            });
+
+            Utils.Coords coordsRight = pathPoint.coords.withXDiff(1);
+            map2d.get(coordsRight).ifPresent(tileCost -> {
+                addIfNotYetAnalyzed(priorityQueue, analyzed, pathCost, coordsRight, tileCost);
+            });
+
+            Utils.Coords coordsLeft = pathPoint.coords.withXDiff(-1);
+            map2d.get(coordsLeft).ifPresent(tileCost -> {
+                addIfNotYetAnalyzed(priorityQueue, analyzed, pathCost, coordsLeft, tileCost);
+            });
+
+            Utils.Coords coordsUp = pathPoint.coords.withYDiff(-1);
+            map2d.get(coordsUp).ifPresent(tileCost -> {
+                addIfNotYetAnalyzed(priorityQueue, analyzed, pathCost, coordsUp, tileCost);
+            });
+        }
+    }
+
+    private void importMap(List<String> lines, Utils.Map2d<Integer> map2d, int i, int j, int width, int height) {
+        int y = 0;
+        for (String line : lines) {
+            for (int x = 0; x < line.length(); x++) {
+                int actualX = width * i + x;
+                int actualY = height * j + y;
+                int v = (Integer.parseInt(line.charAt(x) + "") + i + j);
+                while (v > 9) {
+                    v -= 9;
+                }
+                map2d.put(new Utils.Coords(actualX, actualY), v);
+            }
+            y++;
+        }
+    }
+
+    private void addIfNotYetAnalyzed(PriorityQueue<PathPoint> priorityQueue, Set<Utils.Coords> analyzed, int pathCost, Utils.Coords thisCoords, Integer tileCost) {
+        if (!analyzed.contains(thisCoords)) {
+            priorityQueue.add(new PathPoint(pathCost + tileCost, thisCoords));
+            analyzed.add(thisCoords);
+        }
+    }
+
+
+    private static class PathPoint implements Comparable<PathPoint> {
+        private final int cost;
+        private final Utils.Coords coords;
+
+        public PathPoint(int cost, Utils.Coords coords) {
+            this.cost = cost;
+            this.coords = coords;
+        }
+
+        @Override
+        public int compareTo(PathPoint o) {
+            return Integer.compare(cost, o.cost);
+        }
+
+    }
+}
